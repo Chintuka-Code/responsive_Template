b72aea8a4ac34b7a5bc3b2c3c12389b84c14b65b
mohmad sabban <mohmadsabban123@gmail.com>
2020-05-27 21:35:26 +0530

    First Page

--- Index.js
+++ Index.js
@@ -1,3 +1,29 @@
-console.log('Hi javaScript');
+const slide = document.querySelectorAll('.slide>img');
+let index = 0;
+let time = 3000;
+function show() {
+  slide.forEach((e) => {
+    e.style.display = 'none';
+  });
+  slide[index].style.display = 'block';
+}
 
+function forward(value) {
+  index += value;
+  if (slide.length <= index) {
+    index = 0;
+  }
+  show();
+}
 
+function backward(value) {
+  index -= value;
+  if (index <= 0) {
+    index = slide.length - 1;
+  }
+  show();
+}
+
+const interval = setInterval(() => {
+  forward(1);
+}, time);
